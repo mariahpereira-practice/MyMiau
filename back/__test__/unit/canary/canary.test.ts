57b10e527bfb115be842a444497d82975bceff26
mariah_practice <maria.hpereira@al.infnet.edu.br>
2026-08-12 15:50:45 -0300

    começando a trabalhar com testes automatizados

---
+++ back/__test__/unit/canary/canary.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from '@jest/globals';
+
+describe('Canary Test', () => {
+  test('deve retornar true para true', () => {
+    expect(true).toBeTruthy();
+  });
+});
