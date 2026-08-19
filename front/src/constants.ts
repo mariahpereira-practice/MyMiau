f660971ce88da241a2471137d53140df5a3d8801
mariah_practice <maria.hpereira@al.infnet.edu.br>
2026-08-19 07:48:24 -0300

    removendo o front  - não tive tempo de focar nisso agora

--- front/src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const NAVIGATION_LINKS = [
-    { label: "Início", path: "/" },
-    { label: "Gatos", path: "/gatos" }
-]
-
-export const MINUTES_30 = 30 * 60 * 1000;
--- front/src/utils/calculateTotals.ts
+++
@@ -1,8 +0,0 @@
-import type { CartItem } from "../types";
-
-export function calculateTotals(items: CartItem[]) {
-    return {
-        totalQuantity: items.reduce((sum, item) => sum + item.quantity, 0),
-        totalAmount: items.reduce((sum, item) => sum + item.quantity * item.book.price, 0)
-    }
-}
